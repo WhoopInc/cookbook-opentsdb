48ef70a39d276f119adf5fbaf19b229cf4ba2d94
Nikhil Benesch <nikhil.benesch@gmail.com>
2014-05-27 11:44:41 -0400

    recipes/default: remove unused HBASE_HOME env var config

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,2 @@
-ENV['HBASE_HOME'] = '/usr/lib/hbase'
-
-ruby_block 'add env vars to etc/environment' do
-  block do
-    rc = Chef::Util::FileEdit.new('/etc/environment')
-    rc.insert_line_if_no_match(/^HBASE_HOME/, %(HBASE_HOME="#{ENV['HBASE_HOME']}"))
-    rc.write_file
-  end
-end
-
 include_recipe 'opentsdb::hbase'
 include_recipe 'opentsdb::opentsdb'
