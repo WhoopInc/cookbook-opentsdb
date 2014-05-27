a29bb71ff5acfc6fb56dfb6609ac0793407a05a4
Nikhil Benesch <nikhil.benesch@gmail.com>
2014-05-27 13:59:55 -0400

    .kitchen.yml: extend matrix to Ubuntu 10.04 and Debian Squeeze

--- attributes/java.rb
+++ attributes/java.rb
@@ -1,1 +1,5 @@
-node.default['java']['jdk_version'] = '7'
+if node['platform'] == 'ubuntu' && node['platform_version'].to_f >= 12.04
+  node.default['java']['jdk_version'] = '7'
+else
+  node.default['java']['jdk_version'] = '6'
+end
--- recipes/hbase.rb
+++ recipes/hbase.rb
@@ -3,12 +3,13 @@ require 'shellwords'
 include_recipe 'java::default'
 
 distro = node['lsb']['codename']
+platform = node['platform']
 
 apt_repository 'cloudera_cdh' do
-  uri          "http://archive.cloudera.com/cdh4/ubuntu/#{distro}/amd64/cdh"
+  uri          "http://archive.cloudera.com/cdh4/#{platform}/#{distro}/amd64/cdh"
   distribution "#{distro}-cdh4"
   components   ['contrib']
-  key          "http://archive.cloudera.com/cdh4/ubuntu/#{distro}/amd64/cdh/archive.key"
+  key          "http://archive.cloudera.com/cdh4/#{platform}/#{distro}/amd64/cdh/archive.key"
   arch         'amd64'
   deb_src      true
   action       :add
@@ -19,10 +20,10 @@ end
 end
 
 apt_repository 'cloudera_gplextras' do
-  uri          "http://archive.cloudera.com/gplextras/ubuntu/#{distro}/amd64/gplextras/"
-  distribution "#{node['lsb']['codename']}-gplextras4"
+  uri          "http://archive.cloudera.com/gplextras/#{platform}/#{distro}/amd64/gplextras/"
+  distribution "#{distro}-gplextras4"
   components   ['contrib']
-  key          "http://archive.cloudera.com/gplextras/ubuntu/#{distro}/amd64/gplextras/archive.key"
+  key          "http://archive.cloudera.com/gplextras/#{platform}/#{distro}/amd64/gplextras/archive.key"
   arch         'amd64'
   deb_src      true
   action       :add
