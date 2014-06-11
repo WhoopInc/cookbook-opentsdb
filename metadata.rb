0e39e72c26af18d02a96f17ca5ea665b247ab0af
Nikhil Benesch <nikhil.benesch@gmail.com>
2014-06-11 17:49:27 -0400

    release: v0.2.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'benesch@whoop.com'
 license          'All rights reserved'
 description      'Installs/Configures opentsdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.3'
+version          '0.2.4'
 
 supports 'debian', '= 6'
 supports 'ubuntu', '= 10.04'
